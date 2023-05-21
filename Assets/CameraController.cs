4353a1daaeba7eefd8962c43ad3aff57b47bd235
p00zzel <turghor@gmail.com>
2023-05-21 20:26:55 +0200

    Correct X axis flipping

---
+++ Assets/CameraController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+
+    private void Update()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody2D Player;
-    bool _useRawAxis = true;
+    private Rigidbody2D _rb;
 
+    private Animator _animator;
+
+    private SpriteRenderer _spriteRenderer;
+
+    [SerializeField]
+    private float moveSpeed = 7f;
+
+    private int _lastMove = 0;
+
+    [SerializeField]
+    private float jumpForce = 14f;
+
+    bool _useRawAxis = false;
+    
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hi");
-        Player = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -27,12 +41,34 @@ public class PlayerMovement : MonoBehaviour
             dirX = Input.GetAxis("Horizontal");
         }
 
-        Player.velocity = new Vector2(dirX * 7f, Player.velocity.y);
+        _rb.velocity = new Vector2(dirX * moveSpeed, _rb.velocity.y);
 
         if (Input.GetButtonDown("Jump"))
         {
-            Player.velocity = new Vector2(Player.velocity.x, 14f);
+            _rb.velocity = new Vector2(_rb.velocity.x, jumpForce);
         }
 
+        UpdateAnimations(dirX);
+    }
+
+    private void UpdateAnimations(float dirX)
+    {
+        _animator.SetBool("Running", dirX != 0);
+        if (dirX == 0)
+        {
+            _spriteRenderer.flipX = _lastMove == -1;
+        }
+        if (dirX != 0) 
+        { 
+            if (_lastMove == -1) 
+            {
+                _spriteRenderer.flipX = true;
+            }
+            else if (_lastMove == 1)
+            {
+                _spriteRenderer.flipX = false;
+            }
+            _lastMove = dirX < 0 ? -1 : dirX > 0 ? 1 : 0;
+        }
     }
 }
