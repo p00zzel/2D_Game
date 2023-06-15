6925f4f38737746588f2ef4d86cefb8527f2eeb8
p00zzel <turghor@gmail.com>
2023-06-15 17:01:09 +0200

    Bananas collecting text

--- Assets/Scripts/ItemCollider.cs
+++ Assets/Scripts/ItemCollider.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemCollider : MonoBehaviour
 {
     private int _bananas = 0;
+
+    [SerializeField]
+    private Text bananasText;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -12,6 +16,7 @@ public class ItemCollider : MonoBehaviour
         {
             Destroy(collision.gameObject);
             _bananas++;
+            bananasText.text = "Bananas: " + _bananas;
             Debug.Log("Bananas: " + _bananas);
         }
     }
